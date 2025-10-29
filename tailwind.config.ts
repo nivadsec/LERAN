04fad4aaecd809060c82ee75ed2e79813cd8f7ba
Nivad Sec <nivadsec@gmail.com>
2025-10-29 17:52:25 +0000

    فونت فارسی زیبا استفاده بکن لطفا

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Alegreya', 'serif'],
-        headline: ['Alegreya', 'serif'],
+        body: ['Vazirmatn', 'sans-serif'],
+        headline: ['Vazirmatn', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
