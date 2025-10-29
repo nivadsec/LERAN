80cce995eef63a058862f0301277cbd57583be17
Nivad Sec <nivadsec@gmail.com>
2025-10-29 19:36:25 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/admin/qna/page.tsx
+++ src/app/admin/qna/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Mail, MessageSquare }s from 'lucide-react';
+import { Mail, MessageSquare } from 'lucide-react';
 
 export default function AdminQnAPage() {
   return (
