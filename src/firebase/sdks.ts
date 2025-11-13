f224f79abc9e983671cde1bc60a6fb79537ba50b
Nivad Sec <nivadsec@gmail.com>
2025-11-13 21:09:50 +0000

    Try fixing this error: `Console Error: [2025-11-13T21:09:28.499Z] @fireb

--- src/firebase/sdks.ts
+++ src/firebase/sdks.ts
@@ -1,14 +1,11 @@
 'use client';
 import { FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore'
 
 export function getSdks(firebaseApp: FirebaseApp) {
   const firestore = getFirestore(firebaseApp);
   
-  // Connect to the production proxy
-  connectFirestoreEmulator(firestore, 'lernova-firebase-proxy-production.up.railway.app', 443);
-
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
